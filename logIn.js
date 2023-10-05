863cd19af1dcaf378f459efe8830198943896089
Tulsi-2 <MY_NAME@example.com>
2023-10-05 15:58:02 +0530

    commit

--- logIn.js
+++ logIn.js
@@ -1,18 +1,36 @@
 function login(){
-    // window.location.assign("dashboard.html")
-    let email = document.querySelector('#email').value
-    let pass = document.querySelector('#pass').value
+    
+    const loginemail = document.querySelector('#email').value
+    const pass = document.querySelector('#pass').value
     let parseEmail = JSON.parse(localStorage.getItem('arr'))
     console.log(parseEmail)
+    if(email == '' || pass == ''){
+        alert("Enter a required detail")
+    }
+    else{
     parseEmail.map((item)=>{
-        console.log(item.email)
-        console.log(item.password)
-        if(email== item.email && pass == item.password){
+        if(loginemail== item.email && pass == item.password){
             window.location.assign("dashboard.html")
-        }
-        // else{
-        //     alert("wrong credentials")
-        // }
+            let data = {
+                email: loginemail 
+            }
+            let ary=[]
+            ary.push(data)
+            console.log(data)
+            let m = localStorage.getItem("ary")
+            if (m == null) {
+                const jsonData = JSON.stringify(ary);
+                localStorage.setItem("ary", jsonData);
+              } else {
+                let oldData = JSON.parse(localStorage.getItem("ary"));
+                oldData.push(data);
+                localStorage.setItem("ary", JSON.stringify(oldData));
+                
+                
+              }
 
-    })
+        }
+        
+
+    })}
 }
